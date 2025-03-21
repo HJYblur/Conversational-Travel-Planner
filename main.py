698df50ea19dd0e1f398652a0c18488c7a0f795c
lemonhe@tudelft.nl <lemonhe@tudelft.nl>
2025-03-21 16:25:52 +0100

    Set up the agent pipeline

--- main.py
+++ main.py
@@ -3,15 +3,28 @@ from information_retriever import *
 
 
 def main():
+    # Step1: System initialization
     print('Hello, welcome to the travel recommendation agent!')
     user_init()
     
-    # Information retrieval
+    # Step2-6 should be in a for loop
+    # Step2: Recode the user speech and convert it to text (livekit)
+    
+    # Step3: Summarize short-term memory from the text
+    
+    # Step4.1: Information retrieval from long-term memory(preference)
     model, tokenizer = init_retrieve_model()
     query = 'I want to go to the beach'
-    # TODO: test for single round here, should be replaced by a loop
     retrieved_preference = retrieve(model, tokenizer, query, 'preference')
-    retrieved_event = retrieve(model, tokenizer, query, 'event')
+    
+    # Step4.2(optional): Information retrieval from semantic memory
+    
+    # Step5: Communicate with LLM to generate the response
+    
+    # Step6: Convert the LLM response to speech and output to users
+    
+    # Step7: Thank user for participating, end GUI
+    
     
     
 if __name__ == '__main__':
